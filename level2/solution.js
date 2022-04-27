c7659670d05626be2e3d0d735685c44c90ccc788
jawaher_cherif <jcherif@ubees.com>
2022-04-27 13:30:59 +0200

    create function to calculate the total commission of the user

--- level2/solution.js
+++ level2/solution.js
@@ -1,3 +1,18 @@
+// function to calculate the commission of the user depending on his objective and the total amounts of his deals
+const calculateCommission = (totalAmounts, objective) => {
+  const amountUnder50Percent =
+    totalAmounts > 0.5 * objective ? 0.5 * objective : totalAmounts;
+  const amountOver50Under100Percent =
+    totalAmounts >= objective ? 0.5 * objective : 0;
+  const amountOver100Percent =
+    totalAmounts > objective ? totalAmounts - objective : 0;
+  return (
+    amountUnder50Percent * 0.05 +
+    amountOver50Under100Percent * 0.1 +
+    amountOver100Percent * 0.15
+  );
+};
+
 const data = require("./data/input.json");
 
 const users = data["users"];
