c46893afc4d08a428c0b142dd034e841b22ed968
jawaher_cherif <jcherif@ubees.com>
2022-04-27 13:38:37 +0200

    open the input file and prepare the output

---
+++ level3/solution.py
@@ -0,0 +1,14 @@
+import json
+
+#open the file input.json
+f = open('./data/input.json')
+ 
+#load data from input.json in data 
+data = json.load(f)
+
+#inputs
+users= data["users"]
+deals = data["deals"]
+
+#output
+output = {"commission":[],"deals":[]}
